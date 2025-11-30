28439556a01009a7f1de707c7249834465dd6219
Francesc Ara <cesc78@gmail.com>
2025-11-30 19:11:18 +0100

    add caching to geolocated search

--- src/pages/api/ebirdSpeciesSearch.ts
+++ src/pages/api/ebirdSpeciesSearch.ts
@@ -1,11 +1,22 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
+import { getRedisClient } from '../../client/redis';
+
+const redis = getRedisClient();
+
 const EBIRD_OBS_API_URL = 'https://api.ebird.org/v2/data/obs/geo/recent';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { lat, lng } = req.query;
+  const cacheKey = `ebirdSpeciesSearch:${lat}:${lng}`;
 
   try {
+    let cachedData = await redis.get(cacheKey);
+
+    if (cachedData) {
+      return res.status(200).json(JSON.parse(cachedData));
+    }
+
     const response = await fetch(`${EBIRD_OBS_API_URL}/?lat=${lat || process.env.NEXT_PUBLIC_LAT}&lng=${lng || process.env.NEXT_PUBLIC_LNG}`, {
       headers: {
         'X-eBirdApiToken': process.env.NEXT_PUBLIC_EBIRD_API_TOKEN || '',
@@ -17,6 +28,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     }
 
     const data = await response.json();
+
+    await redis.set(cacheKey, JSON.stringify(data), 'EX', 24 * 60 * 60);
+
     res.status(200).json(data);
   } catch (error) {
     console.error(error);
