0491a6ef918cb81fc1c63872b9f7cf64fdfa1247
Francesc Ara <cesc78@gmail.com>
2026-02-16 20:12:18 +0100

    warm up lambda

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import type { AppProps } from 'next/app';
 import { BirdProvider } from '../contexts/BirdContext';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
+    useEffect(() => {
+        const warmUpLambda = async () => {
+            try {
+                const response = await fetch(`/api/ebirdTaxonFind?bird=ou`);
+                if (!response.ok) {
+                    console.error('Failed to warm up Lambda function:', response.status, response.statusText);
+                }
+            } catch (error) {
+                console.error('Error warming up Lambda function:', error);
+            }
+        };
+
+        warmUpLambda();
+    }, []);
+
     return (
         <BirdProvider>
             <Component {...pageProps} />
